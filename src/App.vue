777d349097d22d4390d9478f9eb5634aeb0dc66b
zymseo <1047832475@qq.com>
2021-12-02 20:31:53 +0800

    test: tree component

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
         </IFormItem>
       </IFormItem>
     </IForm>
+    <ITree v-for="(item, index) in treeData" :key="index" :nodeData="item"></ITree>
   </div>
 </template>
 
@@ -14,13 +15,44 @@
 import IForm from './components/i-form.vue'
 import IFormItem from './components//i-form-item.vue'
 import IInput from './components//i-input.vue'
+import ITree from './components/i-tree.vue'
+
+var cityList = [
+  { name: '北京市', id: 1, pid: 0, level: 0 },
+  { name: '朝阳区', id: 2, pid: 1, level: 1 },
+  { name: '将台', id: 3, pid: 2, level: 2 },
+  { name: '望京', id: 4, pid: 2, level: 2 },
+  { name: '山西省', id: 5, pid: 0, level: 0 },
+  { name: '太原市', id: 6, pid: 5, level: 1 }
+]
 
 export default {
   name: 'App',
   components: {
     IForm,
     IFormItem,
-    IInput
+    IInput,
+    ITree
+  },
+  data () {
+    return {
+      treeData: []
+    }
+  },
+  created () {
+    this.treeData = this.formatCityList(cityList)
+  },
+  methods: {
+    formatCityList (cityList, pid = 0) {
+      const res = []
+      cityList.forEach(city => {
+        if (city.pid === pid) {
+          city.children = this.formatCityList(cityList, city.id)
+          res.push(city)
+        }
+      })
+      return res
+    }
   }
 }
 </script>
---
+++ src/components/i-tree.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="i-tree-container">
+    <div class="i-node-name" :style="{ 'margin-left': nodeData.level * 30 + 'px' }">{{ nodeData.name }}</div>
+    <div v-if="nodeData.children">
+      <i-tree v-for="(item, index) in nodeData.children" :key="index" :nodeData="item"></i-tree>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'i-tree',
+  props: {
+    nodeData: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  }
+}
+</script>
