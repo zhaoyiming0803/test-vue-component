7eba8eb507a6b24326b847ed2f920db839e2f15b
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-12-04 21:37:22 +0800

    chore: format codes

--- src/mixins/EventEmitter.js
+++ src/mixins/EventEmitter.js
@@ -1,3 +1,17 @@
+function dispatch (componentName, eventName, params) {
+  let parent = this.$parent || this.$root
+  let name = parent.$options.name
+  while (parent && (!name || name !== componentName)) {
+    parent = parent.$parent
+    if (parent) {
+      name = parent.$options.name
+    }
+  }
+  if (parent) {
+    parent.$emit.apply(parent, [eventName].concat(params))
+  }
+}
+
 function broadcast (componentName, eventName, params) {
   this.$children.forEach(child => {
     if (child.$options.name === componentName) {
@@ -11,17 +25,7 @@ function broadcast (componentName, eventName, params) {
 export default {
   methods: {
     dispatch (componentName, eventName, params) {
-      let parent = this.$parent || this.$root
-      let name = parent.$options.name
-      while (parent && (!name || name !== componentName)) {
-        parent = parent.$parent
-        if (parent) {
-          name = parent.$options.name
-        }
-      }
-      if (parent) {
-        parent.$emit.apply(parent, [eventName].concat(params))
-      }
+      dispatch.call(this, componentName, eventName, params)
     },
     broadcast (componentName, eventName, params) {
       broadcast.call(this, componentName, eventName, params)
